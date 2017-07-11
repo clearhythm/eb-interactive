7b44b307f35a38b13ec2c0d3a507d64af2a0ddf5
Erik Burns <erik@erikburns.com>
2017-07-10 23:55:37 -0700

    updating main page sections and base styles

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
-var phrases = new Array('Explore the Infinite &#8902;&#8902;&#8902;&#8902;&#8902;', 'Discover your Journey &#8902;&#8902;&#8902;&#8902;&#8902;', 'Genius made Simple &#8902;&#8902;&#8902;&#8902;&#8902;', 'Inspire your Passion &#8902;&#8902;&#8902;&#8902;&#8902;');
+var phrases = new Array('Explore the Infinite &#8902;&#8902;&#8902;&#8902;&#8902;', 'Discover your Pulse &#8902;&#8902;&#8902;&#8902;&#8902;', 'Genius made Simple &#8902;&#8902;&#8902;&#8902;&#8902;', 'Reinvent the Paradigm &#8902;&#8902;&#8902;&#8902;&#8902;', 'Pioneer your Potential &#8902;&#8902;&#8902;&#8902;&#8902;');
 var phrase_count = phrases.length;
 var phrase_index = undefined;
 var phrase_index_prev = phrase_index;
 var phrase_index_prev_2 = phrase_index;
+var phrase_index_prev_3 = phrase_index;
 
 var current_phrase = undefined; // stores our state - if undefined we are on first load or have reached end of current phrase
 var current_word = undefined; // stores our state - if undefined, we are on first load
 var word_index = 0;
 
 var initial_delay = 0; // number of milliseconds to delay initially
-var word_delay = 3000; // number of milliseconds between words
+var word_delay = 4000; // number of milliseconds between words
 var phrase_delay = 0; // number of additional milliseconds between phrases
 
 var title_elem = $(".intro h2");
@@ -17,10 +18,11 @@ if (typeof do_animate_title == "undefined") do_animate_title = true;
 
 // returns a phrase from an array of phrases. keeps track of current phrase to prevent 2 in a row
 function drawPhrase(phrases){
-  while (phrase_index === phrase_index_prev || phrase_index === phrase_index_prev_2 ) { // can only reappear at most every 3rd time
+  while (phrase_index === phrase_index_prev || phrase_index === phrase_index_prev_2 || phrase_index === phrase_index_prev_3 ) { // can only reappear at most every 4th time
     phrase_index = Math.floor(Math.random()*(phrase_count));
   }
   var my_phrase = phrases[phrase_index];
+  phrase_index_prev_3 = phrase_index_prev_2;
   phrase_index_prev_2 = phrase_index_prev;
   phrase_index_prev = phrase_index;
   return my_phrase;
